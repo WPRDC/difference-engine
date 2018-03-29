e58ecd9e9dbeed589891328f48dd88c795c2063e
drw <drw@users.noreply.github.com>
2018-03-29 15:53:27 -0400

    Start to enable resource selection by Difference Engine resource code

--- views.py
+++ views.py
@@ -5,7 +5,7 @@ from django.db import models as django_models
 
 #from django.template import loader
 from django.http import HttpResponse
-import os, sys, csv, json, datetime, time, ckanapi
+import os, sys, re, csv, json, datetime, time, ckanapi
 from pprint import pprint
 from collections import OrderedDict
 
@@ -153,6 +153,17 @@ def get_resource_parameter(site,resource_id,parameter=None,API_key=None):
     except:
         raise RuntimeError("Unable to obtain resource parameter '{}' for resource with ID {}".format(parameter,resource_id))
 
+def fuzzy_find_resources(site,package_id,search_term,API_key=None):
+    # Try to find the resources given the package ID and search term for the resource name.
+    resources = get_package_parameter(site,package_id,'resources',API_key)
+    r_candidates = []
+    for r in resources:
+        if re.search(search_term,r['name'], re.IGNORECASE) is not None:
+            r_candidates.append(r)
+
+    return r_candidates,resources
+            
+
 def find_resource_id(site,package_id,resource_name,API_key=None):
     # Get the resource ID given the package ID and resource name.
     resources = get_package_parameter(site,package_id,'resources',API_key)
@@ -178,7 +189,126 @@ def activate_wormhole(filepath='parameters/settings.json',server='stage'):
 
     return site, API_key, package_id, settings
 
-def get_resource_stuff(site,resource_id,API_key):
+def remove_data_dictionaries(rs):
+    filtered_rs = []
+    for r in rs:
+        if re.search('ictionary',r['name'], re.IGNORECASE) is None:
+            filtered_rs.append(r)
+    return filtered_rs
+
+def find_resource_candidates_from_package(site,package,codes,resource_code):
+    package_id = package['id']
+
+    if len(codes) == 1:
+        # Try to find a unique CSV file in the resources of that package.
+        _, resources = fuzzy_find_resources(site,package_id,search_term='nothing',API_key=None)
+        r_candidates = []
+        for resource in resources:
+            if resource['format'] in ['CSV','csv','.csv']: #'XLSX','XLS']
+                r_candidates.append(resource)
+        if len(r_candidates) == 0:
+            print("No CSV resources found meeting the description '{}'.".format(resource_code))
+        if len(r_candidates) in [0,1]:
+            return r_candidates
+        # There are multiple r_candidates. Filter out data dictionaries.
+        ## (Suppressing data-dictionary results unless they are the unique results.)
+        r_candidates = remove_data_dictionaries(r_candidates)
+
+        if len(r_candidates) == 0:
+            print("No CSV resources found meeting the description '{}'.".format(resource_code))
+        if len(r_candidates) in [0,1]:
+            return r_candidates
+
+        # There are still multiple r_candidates. 
+        #print("Here are the {} resources I found: {}".format(len(r_candidates),r_candidates))
+        #pprint([r['name'] for r in r_candidates])
+        return r_candidates
+
+    elif len(codes) > 1:
+        # Use codes[1] to narrow the search.
+        found_rs, resources = fuzzy_find_resources(site,package_id,search_term=codes[1],API_key=None)
+        print("len(resources) without filtering on codes[1] = {}: {}".format(codes[1],len(resources)))
+
+        print("len(found_rs) after filtering on codes[1] = {}: {}".format(codes[1],len(found_rs)))
+
+        # possibly common block #
+        r_candidates = []
+        for resource in found_rs:
+            if resource['format'] in ['CSV','csv','.csv']: #'XLSX','XLS']
+                r_candidates.append(resource)
+        if len(r_candidates) == 0:
+            print("No CSV resources found meeting the description '{}'.".format(resource_code))
+        if len(r_candidates) in [0,1]:
+            return r_candidates
+        # There are multiple r_candidates. Filter out data dictionaries.
+        ## (Suppressing data-dictionary results unless they are the unique results.)
+        r_candidates = remove_data_dictionaries(r_candidates)
+
+        if len(r_candidates) == 0:
+            print("No CSV resources found meeting the description '{}'.".format(resource_code))
+        if len(r_candidates) in [0,1]:
+            return r_candidates
+        # end possibly common block #
+
+
+        # There are still multiple r_candidates. Use the third part of the code:
+        #print("Here are the {} resources I found: {}".format(len(r_candidates),r_candidates))
+        #pprint([r['name'] for r in r_candidates])
+        return r_candidates
+
+
+
+def decode(resource_code,site,API_key):
+    # Try to find one unique CKAN table described by the given resource_code.
+    try:
+        resource_id = get_resource_parameter(site,resource_code,'id',API_key)
+        assert resource_id == resource_code
+        return resource_id
+    except:
+        # Decode the resource code, which is encoded according to this
+        # human-readable schema:
+        #   <package-name search term>.<resource-name search term>[.<relative month selector>]
+        # Example:
+        #   BigBurgh.services.0
+        codes = resource_code.split('.')
+        package_cache = {'bigburgh': '6e683016-0b93-4776-9d9f-caa03e94edc2'}
+        p_candidates = []
+        if codes[0].lower() in package_cache.keys():
+            package_id = package_cache[codes[0]]
+        else:
+            ckan = ckanapi.RemoteCKAN(site) # Without specifying the apikey field value,
+            # the next line will only return non-private packages.
+            #packages = ckan.action.current_package_list_with_resources(limit=999999)
+            #for p in found_packages:
+            #    if re.search(codes[0],p['title']) is not None:
+            #        p_candidates.append(p)
+            found_packages = ckan.action.package_search(q='title:{}'.format(codes[0]))['results']
+            # Search packages and find the ones that have codes[0] in their titles.
+            pprint([fp['title'] for fp in found_packages])
+
+            p_candidates = found_packages
+            if len(p_candidates) == 0:
+                print("No packages containing '{}' found.".format(codes[0]))
+                return []
+            all_potentials = []
+            for package in p_candidates:
+                r_candidates = find_resource_candidates_from_package(site,package,codes,resource_code)
+                all_potentials += r_candidates
+                print("len(r_candidates) = {}, len(all_potentials) = {}".format(len(r_candidates), len(all_potentials)))
+
+            if len(all_potentials) == 0:
+                return []
+            if len(all_potentials) == 1:
+                return all_potentials[0]['id']
+
+            pprint([r['name'] for r in all_potentials])
+            return all_potentials
+            #r_candidates, resources = fuzzy_find_resources(site,package_id,search_term=codes[1],API_key=None)
+
+def get_resource_stuff(site,resource_code,API_key):
+    resource_id = decode(resource_code,site,API_key)
+    if type(resource_id) == list:
+        return None, None, None, None, [r['name'] for r in resource_id]
     data, schema = get_all_records(site,resource_id,API_key=API_key,chunk_size=5000)
 
     metadata = get_resource_parameter(site,resource_id,None,API_key)
@@ -197,93 +327,105 @@ def get_resource_stuff(site,resource_id,API_key):
 
     field_names = [a['id'] for a in schema]
 
-    return data, schema, data_dict, field_names
+    return data, schema, data_dict, field_names, [resource_id]
 
-def compare(request,resource_id_1=None,resource_id_2=None):
+def compare(request,resource_code_1=None,resource_code_2=None):
     # Get the resources.
     site, API_key, _, _ = activate_wormhole(SETTINGS_FILE,SERVER)
 
-
     # [ ] Start thinking about refactoring this to make one or both of these 
     #     uploaded CSV files.
-    data1, schema1, data_dict_1, field_names1 = get_resource_stuff(site,resource_id_1,API_key)
-    data2, schema2, data_dict_2, field_names2 = get_resource_stuff(site,resource_id_2,API_key)
 
-    pprint(data_dict_1)
+    data1, schema1, data_dict_1, field_names1, candidate_r_ids1 = get_resource_stuff(site,resource_code_1,API_key)
+    data2, schema2, data_dict_2, field_names2, candidate_r_ids2 = get_resource_stuff(site,resource_code_2,API_key)
+   
+    context = { 'candidate_r_ids1': candidate_r_ids1,
+                'candidate_r_ids2': candidate_r_ids2,
+                }
+    if data1 is None and data2 is None:
+        context['error'] = 'Neither {} nor {} could be found.'.format(resource_code_1,resource_code2)
+    elif data1 is None:
+        context['error'] = 'No file could be found under code "{}".'.format(resource_code_1)
+    elif data2 is None:
+        context['error'] = 'No file could be found under code "{}".'.format(resource_code_2)
+    else:
+        pprint(data_dict_1)
 
-    # Compare things using difflib (or whatever).
-    field_table = difflib.HtmlDiff().make_table(
-        fromlines=field_names1,
-        tolines=field_names2,
-        fromdesc='Resource 1 fields',
-        todesc='Resource 2 fields')
+        # Compare things using difflib (or whatever).
+        field_table = difflib.HtmlDiff().make_table(
+            fromlines=field_names1,
+            tolines=field_names2,
+            fromdesc='File 1 fields',
+            todesc='File 2 fields')
 
-    s1 = list(field_names1)
-    s2 = list(field_names2)
-    fn1, fn2 = list(field_names1), list(field_names2) # These should be just the kept and renamed fields.
-    matcher = difflib.SequenceMatcher(None, field_names1, field_names2)
-    for tag, i1, i2, j1, j2 in reversed(matcher.get_opcodes()):
-        if tag == 'delete': # A column was deleted.
-            print('Remove {} from positions [{}:{}]'.format(s1[i1:i2], i1, i2))
-            del s1[i1:i2]
-            del fn1[i1:i2]
+        s1 = list(field_names1)
+        s2 = list(field_names2)
+        fn1, fn2 = list(field_names1), list(field_names2) # These should be just the kept and renamed fields.
+        matcher = difflib.SequenceMatcher(None, field_names1, field_names2)
+        for tag, i1, i2, j1, j2 in reversed(matcher.get_opcodes()):
+            if tag == 'delete': # A column was deleted.
+                print('Remove {} from positions [{}:{}]'.format(s1[i1:i2], i1, i2))
+                del s1[i1:i2]
+                del fn1[i1:i2]
 
-        elif tag == 'equal':
-            print('The sections [{}:{}] of s1 and [{}:{}] of s2 are the same'.format(i1, i2, j1, j2))
+            elif tag == 'equal':
+                print('The sections [{}:{}] of s1 and [{}:{}] of s2 are the same'.format(i1, i2, j1, j2))
 
-        elif tag == 'insert': # A column was added.
-            print('Insert {} from [{}:{}] of s2 into s1 at {}'.format(s2[j1:j2], j1, j2, i1))
-            s1[i1:i2] = s2[j1:j2]
-            del fn2[j1:j2]
+            elif tag == 'insert': # A column was added.
+                print('Insert {} from [{}:{}] of s2 into s1 at {}'.format(s2[j1:j2], j1, j2, i1))
+                s1[i1:i2] = s2[j1:j2]
+                del fn2[j1:j2]
 
-        elif tag == 'replace': # The field names were just changed
-            print('Replace {} from [{}:{}] of s1 with {} from [{}:{}] of s2'.format(s1[i1:i2], i1, i2, s2[j1:j2], j1, j2))
-            s1[i1:i2] = s2[j1:j2]
+            elif tag == 'replace': # The field names were just changed
+                print('Replace {} from [{}:{}] of s1 with {} from [{}:{}] of s2'.format(s1[i1:i2], i1, i2, s2[j1:j2], j1, j2))
+                s1[i1:i2] = s2[j1:j2]
 
-    # [ ] We will probably need to suppress the _id column for some datasets.
-    diff_table = OrderedDict([])
+        # [ ] We will probably need to suppress the _id column for some datasets.
+        diff_table = OrderedDict([])
 
-    for f1,f2 in zip(fn1,fn2):
-        column1 = [str(d[f1]) for d in data1] # We need to cast values to strings so that
-        column2 = [str(d[f2]) for d in data2] # difflib.HtmlDiff can operate on them.
-        diff_table[f1] = difflib.HtmlDiff().make_table(fromlines=column1,
-                tolines=column2,
-                fromdesc='Resource 1: {}'.format(f1),
-                todesc='Resource 2: {}'.format(f2),
+        for f1,f2 in zip(fn1,fn2):
+            column1 = [str(d[f1]) for d in data1] # We need to cast values to strings so that
+            column2 = [str(d[f2]) for d in data2] # difflib.HtmlDiff can operate on them.
+            diff_table[f1] = difflib.HtmlDiff().make_table(fromlines=column1,
+                    tolines=column2,
+                    fromdesc='Resource 1: {}'.format(f1),
+                    todesc='Resource 2: {}'.format(f2),
+                    context=True,
+                    numlines=2,
+                    )
+
+        # Synthesize lists of data with only the selected (kept or renamed) fields.
+        d1, d2 = [], []
+        for row in data1:
+            d1_row = {k:v for (k,v) in row.items() if k in fn1}
+            delta_d1 = OrderedDict([(k,v) for (k,v) in row.items() if k in fn1])
+            d1_flat = ','.join([str(row[f1]) for f1 in fn1])
+            d1.append(d1_flat)
+        for row in data2:
+            d2_flat = ','.join([str(row[f2]) for f2 in fn2])
+            d2.append(d2_flat)
+
+
+        flat_table = difflib.HtmlDiff().make_table(fromlines=d1,
+                tolines=d2,
+                fromdesc='File 1',
+                todesc='File 2',
                 context=True,
                 numlines=2,
                 )
 
-    # Synthesize lists of data with only the selected (kept or renamed) fields.
-    d1, d2 = [], []
-    for row in data1:
-        d1_row = {k:v for (k,v) in row.items() if k in fn1}
-        delta_d1 = OrderedDict([(k,v) for (k,v) in row.items() if k in fn1])
-        d1_flat = ','.join([str(row[f1]) for f1 in fn1])
-        d1.append(d1_flat)
-    for row in data2:
-        d2_flat = ','.join([str(row[f2]) for f2 in fn2])
-        d2.append(d2_flat)
 
-
-    flat_table = difflib.HtmlDiff().make_table(fromlines=d1,
-            tolines=d2,
-            fromdesc='Resource 1',
-            todesc='Resource 2',
-            context=True,
-            numlines=2,
-            )
-
-
-    context = {'data_dict_1': data_dict_1, 'data_dict_2': data_dict_2, 
-            'rows1': len(data1),
-            'columns1': len(field_names1),
-            'rows2': len(data2),
-            'columns2': len(field_names2),
-            'field_table': field_table,
-            'diff_table': diff_table,
-            'flat_table': flat_table
-            }
+        context = {'data_dict_1': data_dict_1, 'data_dict_2': data_dict_2, 
+                'rows1': len(data1),
+                'columns1': len(field_names1),
+                'rows2': len(data2),
+                'columns2': len(field_names2),
+                'field_table': field_table,
+                'diff_table': diff_table,
+                'flat_table': flat_table,
+                'candidate_r_ids1': candidate_r_ids1,
+                'candidate_r_ids2': candidate_r_ids2,
+                }
     return render(request, 'difference_engine/results.html', context)
 
 
